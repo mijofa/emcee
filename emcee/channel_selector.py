b3c21ea4ef9799b0368b85b2b5041b77e7247351
Mike Abrahall <mike@cyber.com.au>
2017-07-04 16:42:49 +1000

    Improve VFS (still uses test data) More preliminary work on selector menu

--- emcee/channel_selector.py
+++ emcee/channel_selector.py
@@ -3,34 +3,61 @@ import emcee.vfs
 
 
 class ChannelSelector(Gtk.ScrolledWindow):
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-        # Only scroll horizontally
-        self.set_policy(hscrollbar_policy=Gtk.PolicyType.AUTOMATIC,
-                        vscrollbar_policy=Gtk.PolicyType.NEVER)
-        self.set_size_request(200, 100)
+    # FIXME: Should this be a "tabbed" widget that switches between a single-button and a scrolledwindow depending on focus status?
+    def __init__(self, station):
+        super().__init__(
+            # Only scroll vertically
+            hscrollbar_policy=Gtk.PolicyType.NEVER,
+            vscrollbar_policy=Gtk.PolicyType.AUTOMATIC,
+        )
 
-        vfs = emcee.vfs.VirtualFilesystem()
+        channels = Gtk.VBox()
+        self.add(channels)
+        for channel in station.channels:
+            channel_button = Gtk.Button.new_with_label(channel.title)
+            channel_button.title = channel.title  # FIXME: Debugging
+            if channel.icon:
+                # If there is a channel icon, add it to the button and remove the text label.
+                channel_button.set_image(Gtk.Image.new_from_file(channel.icon))
+                channel_button.set_label('')  # FIXME: Can I "hide" the label without clearing it?
+
+            channels.pack_start(channel_button, True, True, 0)
+            channel_button.connect('clicked', self.select, channel)
+            channel_button.connect('focus-out-event', lambda btn, direction: print('focus out', btn.title))
+            channel_button.connect('focus-in-event', lambda btn, direction: print('focus in', btn.title, btn.clicked()))
+
+        self.title = station.title  # FIXME: Debugging
+        # FIXME: ScrolledWindow doesn't get focus-in event, only "focus" which doesn't differentiate in/out.
+        #        Is there some way to check "any-child-has-focus" or similar?
+        self.connect('focus', lambda widget, direction: print('focus change of some sort', widget.title, direction))
+
+    def select(self, btn, channel):
+        print('Selected', channel.title, channel.uri),
+
+
+class StationSelector(Gtk.ScrolledWindow):
+    # The UI of this channel selector is a XMB (https://en.wikipedia.org/wiki/XrossMediaBar) style UI.
+    # With stations being across the horizontal bar, and channels within those stations on the vertical bars
+    def __init__(self):
+        # Create the horizontal scroll window
+        super().__init__(
+            hscrollbar_policy=Gtk.PolicyType.AUTOMATIC,
+            vscrollbar_policy=Gtk.PolicyType.NEVER,
+        )
         stations = Gtk.HBox()
         self.add_with_viewport(stations)
-        for s in vfs.list_stations():
-            sw = Gtk.ScrolledWindow()
-            # Only scroll vertically
-            sw.set_policy(hscrollbar_policy=Gtk.PolicyType.NEVER,
-                          vscrollbar_policy=Gtk.PolicyType.AUTOMATIC)
-            stations.add(sw)
-            channels = Gtk.VBox()
-            sw.add(channels)
-            for c in vfs.list_channels(s):
-                b = Gtk.Button.new_with_label(c)
-                b.set_size_request(30, 30)
-                channels.pack_start(b, True, True, 0)
+
+        # Get the list of stations, and create a vertical scroll window for each
+        vfs = emcee.vfs.VirtualFilesystem()
+        for station in vfs.list_stations():
+            stations.add(ChannelSelector(station))
 
 
 if __name__ == '__main__':
     window = Gtk.Window(title='Emcee')
     window.connect("destroy", Gtk.main_quit)  # Quit & cleanup when closed
-    window.add(ChannelSelector())
+    window.add(StationSelector())
+    window.set_size_request(500, 400)
     window.show_all()
 
     Gtk.main()
--- emcee/vfs.py
+++ emcee/vfs.py
@@ -1,25 +1,59 @@
 # This is intended as a wrapper between Emcee and whatever backing store is used behind it.
 # For now however, it just returns the lists I want for testing, I'll implement the actual backing store later
 
+import os
+import collections
+
+Station = collections.namedtuple('Station', 'title icon channels')
+Channel = collections.namedtuple('Channel', 'title icon epg_brief uri')
+
+# Test data.
+data = {'ABC': ['ABC2  ABC4',
+                'ABC3',
+                'ABC HD',
+                'ABC',
+                'ABC Jazz',
+                'ABC News 24',
+                'Double J'],
+        'Internal': ['PPC Local 1', 'PPC Local 2'],
+        'Nine Network Australia': ['9HD',
+                                   '9Life',
+                                   'EXTRA',
+                                   'GEM',
+                                   'GO!',
+                                   'Nine Melbourne',
+                                   'WIN'],
+        'SBS': ['Food Network',
+                'NITV',
+                'SBS HD',
+                'SBS ONE',
+                'SBS Radio 1',
+                'SBS Radio 2',
+                'SBS Radio 3',
+                'SBS TWO'],
+        'Seven Network': ['7 Digital', '7flix', '7mate', '7TWO', 'Racing']}
+
 
 class VirtualFilesystem():
     def list_stations(self):
-        return ['ABC', 'Internal', 'Nine Network Australia', 'SBS', 'Seven Network']
+        stations = []
+        for station_title in sorted(data):
 
-    def list_channels(self, station):
-        if station == "ABC":
-            return ["ABC2  ABC4", "ABC3", "ABC HD", "ABC", "ABC Jazz", "ABC News 24", "Double J"]
-        elif station == "Internal":
-            return ["PPC Local 1", "PPC Local 2"]
-        elif station == "Nine Network Australia":
-            return ["9HD", "9Life", "EXTRA", "GEM", "GO!", "Nine Melbourne", "WIN"]
-        elif station == "SBS":
-            return ["Food Network", "NITV", "SBS HD", "SBS ONE", "SBS Radio 1", "SBS Radio 2", "SBS Radio 3", "SBS TWO"]
-        elif station == "Seven Network":
-            return ["7 Digital", "7flix", "7mate", "7TWO", "Racing"]
+            station_channels = []
+            for channel_title in data.get(station_title):
+                icon_filename = '/home/mike/Videos/tv/{}/{}.gif'.format(station_title, channel_title)
+                station_channels.append(Channel(
+                    title=channel_title,
+                    icon=icon_filename if os.path.isfile(icon_filename) else None,  # FIXME: Make this use a file-object or similar
+                    epg_brief=('now', 'next'),
+                    uri='file:///dev/null',
+                ))
 
-    def get_icon(self, channel, station):
-        raise NotImplementedError()
+            icon_filename = '/home/mike/Videos/tv/{}/folder.gif'.format(station_title)
+            stations.append(Station(
+                title=station_title,
+                icon=icon_filename if os.path.isfile(icon_filename) else None,  # FIXME: Make this use a file-object or similar
+                channels=station_channels,
+            ))
 
-    def get_info(self, channel, station):
-        raise NotImplementedError()
+        return stations
