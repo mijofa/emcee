8a3dc4973ef01e77283d22b6c5c94d00ad576008
Mike Abrahall <mike@cyber.com.au>
2017-07-06 17:38:46 +1000

    Channel selector works much better now

--- emcee/channel_selector.py
+++ emcee/channel_selector.py
@@ -1,38 +1,110 @@
-from gi.repository import Gtk
+from gi.repository import Gtk, GObject, Gdk
 import emcee.vfs
 
+# Note, BUTTON_SIZE should be slightly larger than the icon images themselves.
+# I'm not really sure how much larger as I never bothered to look for the numbers,
+# but the Button has it's own border that it puts around the icons that needs to be accounted for.
+BUTTON_SIZE = (150, 150)  # FIXME: This is based on the current size of the channel gifs
 
-class ChannelSelector(Gtk.ScrolledWindow):
-    # FIXME: Should this be a "tabbed" widget that switches between a single-button and a scrolledwindow depending on focus status?
+# FIXME: Move this stylesheet out into a CSS file and import that as a theme in the application
+style_provider = Gtk.CssProvider()
+css = b"""
+GtkWindow {
+   background-color: darkblue;
+}
+GtkButton {
+   border-radius: 15px;
+}
+/* This is when the  mouse is hovering over the button
+ * .button:hover {
+ *
+ * }
+ */
+/* This is when the button is clicked while the mouse is hovering over it
+ * .button:hover:active {
+ *
+ * }
+ */
+.button:focus {
+   /* Background colour can not be set on a GtkButton without clearing the background image */
+   background-image: none;
+   background-color: yellow;
+}
+"""
+style_provider.load_from_data(css)
+Gtk.StyleContext.add_provider_for_screen(
+    Gdk.Screen.get_default(),
+    style_provider,
+    Gtk.STYLE_PROVIDER_PRIORITY_APPLICATION
+)
+
+
+class ImageOrLabelButton(Gtk.Button):
+    def __init__(self, label_str, image_path=None):
+        super().__init__()
+        if image_path:
+            self.set_image(Gtk.Image.new_from_file(image_path))
+        else:
+            self.set_label(label_str)
+        self.set_size_request(*BUTTON_SIZE)
+
+
+class StationColumn(Gtk.Stack):
+    # Can I use Gtk.EventBox to get focus-in and focus-out events?
+    __gsignals__ = {
+        'select-station': (GObject.SIGNAL_RUN_LAST, GObject.TYPE_NONE, ()),
+    }
+
     def __init__(self, station):
-        super().__init__(
+        self.station = station
+
+        super().__init__()
+
+        # The stack that displays when not selected
+        station_box = Gtk.VBox()
+        self.add_named(station_box, "unselected")
+        # Empty widget of the same size as the button itself,
+        # this allows the selected station to have an extra button above the selected one.
+        spacer = Gtk.DrawingArea()
+        spacer.set_size_request(*BUTTON_SIZE)
+        station_box.pack_start(spacer, expand=False, fill=False, padding=0)
+        # The button for selecting the station itself
+        station_btn = ImageOrLabelButton(station.title, station.icon)
+        station_btn.connect('clicked', self.select_station)
+        station_box.pack_start(station_btn, expand=False, fill=False, padding=0)
+
+        # FIXME: Remove the upper/lower bounds on this scroller, set the step & page size to match the BUTTON_SIZE.
+        #        Somehow process the scrolling using arrow keys.
+        #        Note sure whether I should bind the arrows to scrolling then do things when scrolling happens,
+        #        or steal the arrows and do the scrolling myself.
+        scroller = Gtk.ScrolledWindow(
             # Only scroll vertically
             hscrollbar_policy=Gtk.PolicyType.NEVER,
             vscrollbar_policy=Gtk.PolicyType.AUTOMATIC,
         )
+        # FIXME: This is a workaround for PolicyType.EXTERNAL not coming in until 3.16
+        #        https://lazka.github.io/pgi-docs/Gtk-3.0/enums.html#Gtk.PolicyType.AUTOMATIC
+        scroller.get_vscrollbar().set_visible(False)
+        self.add_named(scroller, "selected")
+#        station_btn.set_can_focus(False)
 
-        channels = Gtk.VBox()
-        self.add(channels)
+        channels_box = Gtk.VBox()
+        scroller.add(channels_box)
         for channel in station.channels:
-            channel_button = Gtk.Button.new_with_label(channel.title)
-            channel_button.title = channel.title  # FIXME: Debugging
-            if channel.icon:
-                # If there is a channel icon, add it to the button and remove the text label.
-                channel_button.set_image(Gtk.Image.new_from_file(channel.icon))
-                channel_button.set_label('')  # FIXME: Can I "hide" the label without clearing it?
+            channel_btn = ImageOrLabelButton(channel.title, channel.icon)
+            channel_btn.connect('clicked', self.select_channel, channel)
+            channels_box.pack_start(channel_btn, expand=False, fill=False, padding=0)
 
-            channels.pack_start(channel_button, True, True, 0)
-            channel_button.connect('clicked', self.select, channel)
-            channel_button.connect('focus-out-event', lambda btn, direction: print('focus out', btn.title))
-            channel_button.connect('focus-in-event', lambda btn, direction: print('focus in', btn.title, btn.clicked()))
+        # This spacer keeps all the buttons from growing larger than BUTTON_SIZE when the window grows.
+        spacer = Gtk.DrawingArea()
+        channels_box.pack_start(spacer, expand=True, fill=True, padding=0)
 
-        self.title = station.title  # FIXME: Debugging
-        # FIXME: ScrolledWindow doesn't get focus-in event, only "focus" which doesn't differentiate in/out.
-        #        Is there some way to check "any-child-has-focus" or similar?
-        self.connect('focus', lambda widget, direction: print('focus change of some sort', widget.title, direction))
+    def select_station(self, _):
+        print('Selected', self.station.title),
+        self.emit('select-station')
 
-    def select(self, btn, channel):
-        print('Selected', channel.title, channel.uri),
+    def select_channel(self, _, channel):
+        print('Play', channel.title, channel.uri),
 
 
 class StationSelector(Gtk.ScrolledWindow):
@@ -41,23 +113,43 @@ class StationSelector(Gtk.ScrolledWindow):
     def __init__(self):
         # Create the horizontal scroll window
         super().__init__(
+            # Only scroll horizontally, the StationColumn has it's own vertical scroller
             hscrollbar_policy=Gtk.PolicyType.AUTOMATIC,
             vscrollbar_policy=Gtk.PolicyType.NEVER,
         )
-        stations = Gtk.HBox()
-        self.add_with_viewport(stations)
+        # FIXME: This is a workaround for PolicyType.EXTERNAL not coming in until 3.16
+        #        https://lazka.github.io/pgi-docs/Gtk-3.0/enums.html#Gtk.PolicyType.AUTOMATIC
+        self.get_hscrollbar().set_visible(False)
 
-        # Get the list of stations, and create a vertical scroll window for each
+        # Despite being a bunch of VBoxes inside a HBox, Grid will not work in this case.
+        # This is because the size of the 2nd column is intended to simply take up all the window space it can,
+        # this can't be done with the Grid widget
+        box = Gtk.HBox()
+        self.add(box)
+
         vfs = emcee.vfs.VirtualFilesystem()
+        self.columns = []
         for station in vfs.list_stations():
-            stations.add(ChannelSelector(station))
+            c = StationColumn(station)
+            c.connect('select-station', self.change_station)
+
+            self.columns.append(c)
+            box.pack_start(c, expand=False, fill=False, padding=0)
+
+    def change_station(self, station_widget):
+        for c in self.columns:
+            if c != station_widget:
+                c.set_visible_child_name("unselected")
+            else:
+                c.set_visible_child_name("selected")
 
 
 if __name__ == '__main__':
     window = Gtk.Window(title='Emcee')
     window.connect("destroy", Gtk.main_quit)  # Quit & cleanup when closed
-    window.add(StationSelector())
-    window.set_size_request(500, 400)
+    ss = StationSelector()
+    window.add(ss)
+    window.set_size_request(BUTTON_SIZE[0] * 3, BUTTON_SIZE[1] * 3)
     window.show_all()
 
     Gtk.main()
